49e1d62be5b6438ae143e9592e151dd058407a01
Noah D. Brenowitz <nbren12@gmail.com>
2020-11-09 23:42:02 -0800

    Read initialization code directly
    This simplifies the packaging of this code, since no python module needs
    to be installed with it.

--- src/builder.py
+++ src/builder.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import cffi
+
 ffibuilder = cffi.FFI()
 
 header = """
@@ -13,45 +14,20 @@ extern int set_state_scalar(char *, double *);
 extern int call_function(char *, char *);
 """
 
-module = """
-from my_plugin import ffi
-import callpy as module
-
-@ffi.def_extern(error=1)
-def set_state_scalar(*args):
-    module.set_state_scalar(args, ffi=ffi)
-    return 0
-
-@ffi.def_extern(error=1)
-def set_state_py(*args):
-    module.set_state(args, ffi=ffi)
-    return 0
-
-@ffi.def_extern(error=1)
-def set_state_char(*args):
-    module.set_state_char(args, ffi=ffi)
-    return 0
-
-@ffi.def_extern(error=1)
-def get_state_py(*args):
-    module.get_state(args, ffi=ffi)
-    return 0
-
-@ffi.def_extern(error=1)
-def call_function(*args):
-    a, b = [ffi.string(ch).decode('UTF-8') for ch in args]
-    module.call_function(a, b)
-    return 0
-"""
-
 with open("plugin.h", "w") as f:
     f.write(header)
 
 ffibuilder.embedding_api(header)
 
-ffibuilder.set_source("my_plugin", r'''
+ffibuilder.set_source(
+    "my_plugin",
+    r"""
     #include "plugin.h"
-''')
+""",
+)
 
-ffibuilder.embedding_init_code(module)
-ffibuilder.compile(target="libplugin.so", verbose=True)
+with open(sys.argv[1]) as f:
+    ffibuilder.embedding_init_code(f.read())
+
+ffibuilder.emit_c_code("plugin.c")
+# ffibuilder.compile(target="libplugin.so", verbose=True)
