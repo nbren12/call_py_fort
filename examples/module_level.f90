f547a2f39e84fbb51e44e3a2f28b829642cacddf
Noah D. Brenowitz <nbren12@gmail.com>
2022-05-14 11:48:33 -0700

    remove dangling file

--- examples/module_level.f90
+++
@@ -1,9 +0,0 @@
-program example
-use callpy_mod
-implicit none
-
-call call_function("initialize_stuff", "function")
-call call_function("initialize_stuff", "function")
-call call_function("initialize_stuff", "function")
-
-end program example
